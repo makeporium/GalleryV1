014f0afaf3a2694492bf93380c762ce2811cdbcd
makeporium <makeporium@gmail.com>
2026-05-03 16:17:21 +0530

    Automated migration via orchestrator

--- server/src/index.js
+++ server/src/index.js
@@ -2,8 +2,13 @@ const app = require("./app");
 const env = require("./config/env");
 const { sequelize } = require("./models");
 
+const orchestrateMigration = require("./scripts/migrationOrchestrator");
+
 async function start() {
   try {
+    // Run migration if needed
+    await orchestrateMigration();
+    
     await sequelize.authenticate();
     await sequelize.sync();
     app.listen(env.port, () => {
